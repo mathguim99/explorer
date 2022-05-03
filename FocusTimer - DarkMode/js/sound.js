fc239afe6cabcb561a5ddac3b6d9e1d46d6bead3
mathguim99 <104865002+mathguim99@users.noreply.github.com>
2022-05-03 17:07:33 -0300

    Update sound.js

--- FocusTimer - DarkMode/js/sound.js
+++ FocusTimer - DarkMode/js/sound.js
@@ -2,8 +2,6 @@ export default function Sound() {
     const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
     const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
 
-    buttonPressAudio.volume = 0
-
     function buttonPress() {
         buttonPressAudio.play()
     }
